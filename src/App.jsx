6b8e930f02aecc6079fba34c5862f3822cd80e8f
Vinicius Teixeira <viniciusds.teixeira03@gmail.com>
2025-11-21 20:55:10 -0300

    feat: adiciona container principal

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,11 @@
 import React from 'react'
+import Container from './components/Container'
 
 const App = () => {
   return (
-    <div className='bg-red-500'>Olá mundo</div>
+    <div>
+      <Container></Container>
+    </div>
   )
 }
 
---
+++ src/components/Container.jsx
@@ -0,0 +1,9 @@
+const Container = () => {
+  return (
+    <div className="w-[90%] h-[90vh] mx-auto flex mt-10">
+      
+    </div>
+  );
+};
+
+export default Container;
