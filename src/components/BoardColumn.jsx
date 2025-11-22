3282d01f47e4330903296374822018644b573302
Vinicius Teixeira <viniciusds.teixeira03@gmail.com>
2025-11-21 21:56:34 -0300

    feat: adicionada prop button em BoardColumn

--- src/components/BoardColumn.jsx
+++ src/components/BoardColumn.jsx
@@ -1,6 +1,6 @@
-const BoardColumn = ({ title = "Column", counter = 0 }) => {
+const BoardColumn = ({ title = "Column", counter = 0, button = false }) => {
   return (
-    <div className="h-full w-[33%] bg-white rounded-xl shadow p-4">
+    <div className="h-full w-[33%] bg-white rounded-xl shadow p-4 flex flex-col">
       <div className="flex items-center justify-between mb-3">
         <h2 className="text-lg font-semibold">{title}</h2>
         <span className="px-2 w-[30px] py-1 text-xs text-center font-medium bg-blue-400 text-gray-700 rounded-3xl">
@@ -8,7 +8,19 @@ const BoardColumn = ({ title = "Column", counter = 0 }) => {
         </span>
       </div>
       <hr className="text-gray-400" />
-      <div>{/* Column content goes here */}</div>
+
+      {button && (
+        <div className="mt-3">
+          <button
+            type="button"
+            className="w-full h-[45px] bg-green-500 text-white px-3 py-2 rounded-xl shadow-sm cursor-pointer"
+          >
+            + Adicionar
+          </button>
+        </div>
+      )}
+
+      <div className="flex-1">{/* Column content goes here */}</div>
     </div>
   );
 };
