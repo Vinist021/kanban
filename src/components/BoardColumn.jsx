54733b1aa46c6f69660a07bb689bbce6c25bc151
Vinicius Teixeira <viniciusds.teixeira03@gmail.com>
2025-11-21 21:51:29 -0300

    feat: adicionado contador de tarefas

--- src/components/BoardColumn.jsx
+++ src/components/BoardColumn.jsx
@@ -1,11 +1,16 @@
-import React from 'react'
-
-const BoardColumn = () => {
+const BoardColumn = ({ title = "Column", counter = 0 }) => {
   return (
-    <div className='h-full w-[33%] bg-white rounded-xl shadow'>
-        aaa
+    <div className="h-full w-[33%] bg-white rounded-xl shadow p-4">
+      <div className="flex items-center justify-between mb-3">
+        <h2 className="text-lg font-semibold">{title}</h2>
+        <span className="px-2 w-[30px] py-1 text-xs text-center font-medium bg-blue-400 text-gray-700 rounded-3xl">
+          {counter}
+        </span>
+      </div>
+      <hr className="text-gray-400" />
+      <div>{/* Column content goes here */}</div>
     </div>
-  )
-}
+  );
+};
 
-export default BoardColumn
+export default BoardColumn;
--- src/components/Container.jsx
+++ src/components/Container.jsx
@@ -6,9 +6,9 @@ const Container = () => {
     <div className="w-[90%] h-[90vh] mx-auto flex flex-col items-center mt-10 gap-6">
       <SearchInput></SearchInput>
       <div className="w-full h-[80%] flex gap-6">
-        <BoardColumn></BoardColumn>
-        <BoardColumn></BoardColumn>
-        <BoardColumn></BoardColumn>
+        <BoardColumn title="📌 A fazer" counter={4} />
+        <BoardColumn title="⚙️ Em Progresso" counter={2} />
+        <BoardColumn title="✅ Concluído" counter={0} />
       </div>
     </div>
   );
