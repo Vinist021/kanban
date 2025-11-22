4d52af985fbda5a7b0a4426dca14d18177dbd73a
Vinicius Teixeira <viniciusds.teixeira03@gmail.com>
2025-11-21 21:32:55 -0300

    feat: adicionadas colunas

---
+++ src/components/BoardColumn.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const BoardColumn = () => {
+  return (
+    <div className='h-full w-[33%] bg-white rounded-xl shadow'>
+        aaa
+    </div>
+  )
+}
+
+export default BoardColumn
--- src/components/Container.jsx
+++ src/components/Container.jsx
@@ -1,9 +1,15 @@
+import BoardColumn from "./BoardColumn";
 import SearchInput from "./SearchInput";
 
 const Container = () => {
   return (
-    <div className="w-[90%] h-[90vh] mx-auto flex mt-10">
-        <SearchInput></SearchInput>
+    <div className="w-[90%] h-[90vh] mx-auto flex flex-col items-center mt-10 gap-6">
+      <SearchInput></SearchInput>
+      <div className="w-full h-[80%] flex gap-6">
+        <BoardColumn></BoardColumn>
+        <BoardColumn></BoardColumn>
+        <BoardColumn></BoardColumn>
+      </div>
     </div>
   );
 };
